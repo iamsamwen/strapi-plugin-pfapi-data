14e83538d1c92e7e3d6c7fb7e28bdd1c3adb5dfb
Sam Wen <samw@ritani.com>
2022-07-06 08:33:39 -0400

    update

--- data/post-install.js
+++ data/post-install.js
@@ -3,39 +3,13 @@
 const fs = require('fs-extra');
 const node_path = require('path');
 const unzip = require('./unzip');
+const { find_project_root } = require('strapi-pfapi');
 
 (async () => {
 
-    let root;
-    
-    let path = __dirname;
-    let index = path.indexOf('node_modules');
-    if (index !== -1) {
-        root = path.slice(0, index - 1);
-    } else {
-        path = require.main.filename;
-        index = path.indexOf('node_modules');
-        if (index !== -1) {
-            root = path.slice(0, index - 1);
-        } else {
-            const data_path = '/src/plugins/pfapi-data/data';
-            if (__dirname.endsWith(data_path)) {
-                root = __dirname.slice(0, __dirname.length - data_path.length);
-            } else {
-                console.log('strapi project root not found');
-                return;
-            }
-        }
-    }
-    
-    if (!fs.existsSync(node_path.join(root, 'src', 'admin'))) {
-        console.log('not a strapi project root');
-        return;
-    }
-    if (!fs.existsSync(node_path.join(root, 'src', 'api'))) {
-        console.log('not a strapi project root');
-        return;
-    }
+    const root = find_project_root('src/plugins/pfapi-data/data', __dirname);
+
+    if (!root) return;
 
     const world_city_path = node_path.join(root, 'src', 'api', 'world-city');
 
